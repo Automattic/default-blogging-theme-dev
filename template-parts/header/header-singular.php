eb3dffa06abd42c077f36ae7f3e1b4f067f0cb8e
Ernesto Méndez <mendezcode@gmail.com>
2018-09-10 20:14:29 -0400

    Post header image implemented – comment avatars coming down the road

--- template-parts/header/header-singular.php
+++ template-parts/header/header-singular.php
@@ -1,10 +1,5 @@
-<?php
-$background_image_style = has_post_thumbnail()
-	? sprintf( ' style="background-image: url(%s);"', esc_url( get_the_post_thumbnail_url( get_the_ID(), 'post-thumbnail' ) ) )
-	: null;
-?>
-<header id="masthead" class="<?php echo has_post_thumbnail() ? 'site-header cover-image' : 'site-header' ?>"<?php echo $background_image_style; ?>>
-	<div class="layout-wrap">
+<header id="masthead" class="<?php echo has_post_thumbnail() ? 'site-header cover-image' : 'site-header' ?>">
+	<div class="site-branding-container layout-wrap">
 		<?php if ( has_custom_logo() ) : ?>
 			<div class="site-logo"><?php the_custom_logo(); ?></div>
 		<?php endif; ?>
@@ -23,7 +18,6 @@ $background_image_style = has_post_thumbnail()
 	</div><!-- .singular-post-meta -->
 
 	<?php endif; ?>
-
 </header><!-- #masthead -->
 
 <div class="layout-wrap">
